214f8b9945007aa9c026b08d6a8fe47a6f648fde
MukeshKumarRattu <64393938+MukeshKumarRattu@users.noreply.github.com>
2020-12-16 22:19:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/soc_macmini_15/musicplayer/Activity/MainActivity.java
@@ -0,0 +1,249 @@
+package com.example.soc_macmini_15.musicplayer.Activity;
+
+import android.Manifest;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SearchView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.soc_macmini_15.musicplayer.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Menu menu;
+
+    private ImageButton imgBtnPlayPause, imgbtnReplay, imgBtnPrev, imgBtnNext, imgBtnSetting;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private SeekBar seekbarController;
+    private DrawerLayout mDrawerLayout;
+    private TextView tvCurrentTime, tvTotalTime;
+
+
+    private int currentPosition;
+    private String searchText = "";
+
+    private boolean checkFlag = false, repeatFlag = false, playContinueFlag = false, favFlag = true, playlistFlag = false;
+    private final int MY_PERMISSION_REQUEST = 100;
+    private int allSongLength;
+
+    MediaPlayer mediaPlayer;
+    Handler handler;
+    Runnable runnable;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+
+    }
+
+    private void init() {
+        imgBtnPrev = findViewById(R.id.img_btn_previous);
+        imgBtnNext = findViewById(R.id.img_btn_next);
+        imgbtnReplay = findViewById(R.id.img_btn_replay);
+        imgBtnSetting = findViewById(R.id.img_btn_setting);
+
+        tvCurrentTime = findViewById(R.id.tv_current_time);
+        tvTotalTime = findViewById(R.id.tv_total_time);
+        FloatingActionButton refreshSongs = findViewById(R.id.btn_refresh);
+        seekbarController = findViewById(R.id.seekbar_controller);
+        viewPager = findViewById(R.id.songs_viewpager);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        imgBtnPlayPause = findViewById(R.id.img_btn_play);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        handler = new Handler();
+        mediaPlayer = new MediaPlayer();
+
+        toolbar.setTitleTextColor(getResources().getColor(R.color.text_color));
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.menu_icon);
+
+        imgBtnNext.setOnClickListener(this);
+        imgBtnPrev.setOnClickListener(this);
+        imgbtnReplay.setOnClickListener(this);
+        refreshSongs.setOnClickListener(this);
+        imgBtnPlayPause.setOnClickListener(this);
+        imgBtnSetting.setOnClickListener(this);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                switch (item.getItemId()) {
+                    case R.id.nav_about:
+                        about();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+
+
+
+    private void about() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.about))
+                .setMessage(getString(R.string.about_text))
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+        SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchText = newText;
+                return true;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(Gravity.START);
+                return true;
+            case R.id.menu_search:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_favorites:
+                if (checkFlag)
+                    if (mediaPlayer != null) {
+                        if (favFlag) {
+                            Toast.makeText(this, "Added to Favorites", Toast.LENGTH_SHORT).show();
+                            item.setIcon(R.drawable.ic_favorite_filled);
+                            favFlag = false;
+                        } else {
+                            item.setIcon(R.drawable.favorite_icon);
+                            favFlag = true;
+                        }
+                    }
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.img_btn_play:
+                if (checkFlag) {
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                        imgBtnPlayPause.setImageResource(R.drawable.play_icon);
+                    } else if (!mediaPlayer.isPlaying()) {
+                        mediaPlayer.start();
+                        imgBtnPlayPause.setImageResource(R.drawable.pause_icon);
+                    }
+                } else {
+                    Toast.makeText(this, "Select the Song ..", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.btn_refresh:
+                Toast.makeText(this, "Refreshing", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.img_btn_replay:
+
+                if (repeatFlag) {
+                    Toast.makeText(this, "Replaying Removed..", Toast.LENGTH_SHORT).show();
+                    mediaPlayer.setLooping(false);
+                    repeatFlag = false;
+                } else {
+                    Toast.makeText(this, "Replaying Added..", Toast.LENGTH_SHORT).show();
+                    mediaPlayer.setLooping(true);
+                    repeatFlag = true;
+                }
+                break;
+            case R.id.img_btn_previous:
+                if (checkFlag) {
+                    if (mediaPlayer.getCurrentPosition() > 10) {
+                        if (currentPosition - 1 > -1) {
+                            currentPosition = currentPosition - 1;
+                        } else {
+                        }
+                    } else {
+                    }
+                } else {
+                    Toast.makeText(this, "Select a Song . .", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.img_btn_next:
+                if (checkFlag) {
+
+                } else {
+                    Toast.makeText(this, "Select the Song ..", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.img_btn_setting:
+                if (!playContinueFlag) {
+                    playContinueFlag = true;
+                    Toast.makeText(this, "Loop Added", Toast.LENGTH_SHORT).show();
+                } else {
+                    playContinueFlag = false;
+                    Toast.makeText(this, "Loop Removed", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/soc_macmini_15/musicplayer/Adapter/ViewPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.soc_macmini_15.musicplayer.Adapter;
+
+import android.content.ContentResolver;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private ContentResolver contentResolver;
+    private String title[] = {"All SONGS","ONLINE PLAYLIST", "FAVORITES"};
+
+    public ViewPagerAdapter(FragmentManager fm, ContentResolver contentResolver) {
+        super(fm);
+        this.contentResolver = contentResolver;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return null;
+            case 1:
+                return null;
+            case 2:
+                return null;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return title[position];
+    }
+}
